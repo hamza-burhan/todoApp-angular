85214528806ab1a5baa5a3b412600a3d6d6071d0
Hamza Burhan <hamzaburhan0985@gmail.com>
2023-07-21 18:54:31 +0500

    add splash screen and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,27 @@ const config: CapacitorConfig = {
   webDir: 'dist/front-end',
   server: {
     androidScheme: 'https'
-  }
+  },
+  
+    "plugins": {
+      "SplashScreen": {
+      //   "launchShowDuration": 3000,
+      //   "launchAutoHide": true,
+      //   "launchFadeOutDuration": 3000,
+        "backgroundColor": "#000000",
+      //   "androidSplashResourceName": "splash",
+      //   "androidScaleType": "CENTER_CROP",
+        "showSpinner": true,
+      //   "androidSpinnerStyle": "large",
+      //   "iosSpinnerStyle": "small",
+      //   "spinnerColor": "#999999",
+      //   "splashFullScreen": true,
+      //   "splashImmersive": true,
+      //   "layoutName": "launch_screen",
+      //   "useDialog": true
+      }
+    }
+  
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontEnd';
+  constructor() {
+     SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+    });
+}
 }
